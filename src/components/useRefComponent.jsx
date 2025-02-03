7340a3984e7ec9481ee2eb24ec3d5365eeda4756
Milad-Aijazi <syedmilad245@gmail.com>
2025-02-03 20:00:05 +0500

    added useRef component.

---
+++ src/components/useRefComponent.jsx
@@ -0,0 +1,11 @@
+
+const useRefComponent = () =>{
+    return (
+        <div>
+            <button type="button" >button from parent</button>
+            <button type="button" >button from child</button>
+        </div>
+    )
+}
+
+export default useRefComponent
